ec8dcda472f8aee5136abd060742c4fbf8fac5f6
Yannis-Borel <yannis.borel@edu.univ-fcomte.fr>
2024-03-13 16:07:48 +0100

    uhuh

---
+++ src/components/AnimationText.vue
@@ -0,0 +1,132 @@
+<script setup lang="ts">
+defineProps<{
+    content?: string
+}>()
+import { onMounted } from 'vue';
+
+import { gsap } from "gsap/dist/gsap";
+
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+
+
+onMounted(() => {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timelinetextline1 = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#line1",
+            start: "top bottom",
+            end:"bottom+=850px bottom",
+            scrub: true,
+            markers: false,
+        },
+    })
+
+    timelinetextline1.to("#line1", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+
+
+    timelinetextline1.to("#line2", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+    timelinetextline1.to("#line3", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+    timelinetextline1.to("#line4", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+    timelinetextline1.to("#line5", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+
+    timelinetextline1.to("#line6", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+    timelinetextline1.to("#line7", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+    timelinetextline1.to("#line8", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+    timelinetextline1.to("#line9", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+    timelinetextline1.to("#line10", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+    timelinetextline1.to("#line11", {
+       backgroundPosition: "0% 100%",
+       ease: "power1.inOut",
+       duration: 300000
+    })
+
+
+
+
+
+});
+</script>
+<template>
+
+<div class="flex w-full justify-center items-center flex-col border-dotted border-2 rounded-[100px]   mx-4 my-48  text-[16px] sm:text-[20px] md:text-[24px] lg:text-[30px] font-bold  " >
+    
+    <div class="flex justify-start items-start w-4/5 ml-[15%] flex-col text-start">
+        <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line1">Passionné par le </p>
+   <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line2"> développement et la conception </p>
+ 
+        <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line3">de solutions web innovantes, je suis</p>
+        <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line4">
+   un étudiant dynamique de 19 ans en deuxième année de BUT MMI, </p>
+        <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line5">   </p>
+  <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line6">
+   où j'affûte mes compétences en design et en programmation.</p>
+     <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line7">
+   Aspirant développeur web full stack,</p>
+       <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line8">
+   j'ai une soif d'apprendre constante</p>
+   <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line9">
+   et un engagement profond envers</p>
+   <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line10">
+    le code propre et les pratiques</p>
+   <p class="text-3xl text-balance  text-titre font-black" style="background: linear-gradient(90deg,#FFFFFF 50%,rgba(6,4,26,.2) 0); background-position-x: 100%;background-size: 200% 100%;   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;" id="line11">
+   de développement agile.</p>
+    
+    </div>
+</div>
+
+
+</template>`
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,86 +1,90 @@
 <template>
-    <LoadingScreen />
-    <div class="min-h-screen bg-header-degrade flex flex-col relative">
-      <Header class="absolute" />
-      <main class="absolute inset-0 flex justify-end items-center">
-    <section class="mr-[200px] mt-[-300px]"> <!-- Adjust the margin as needed -->
-      <!-- Conteneur de texte aligné à droite -->
-      <div>
-        <h1 class="text-black text-[96px]  font-normal leading-normal tracking-wide" style="font-family: 'Coco-Bold';">Développeur web</h1>
-        <h2 class="text-black text-[48px] font-normal leading-normal tracking-[5.76px] ml-[90px]" style="font-family: 'POLYA';">Full-stack</h2>
-      </div>
-    </section>
-  </main>
+  <LoadingScreen />
+  <div class="min-h-screen bg-header-degrade flex flex-col relative">
+    <Header class="absolute" />
+    <main class="absolute inset-0 flex justify-end items-center">
+      <section class="mr-[200px] mt-[-300px]">
+        <!-- Conteneur de texte aligné à droite -->
+        <div>
+          <h1 class="text-black text-[96px] font-normal leading-normal tracking-wide" style="font-family: 'Coco-Bold';">Développeur web</h1>
+          <h2 class="text-black text-[48px] font-normal leading-normal tracking-[5.76px] ml-[90px]" style="font-family: 'POLYA';">Full-stack</h2>
+        </div>
+      </section>
+    </main>
 
-<div class="relative overflow-hidden" style="height: 90vh;">
-  <!-- Image de fond -->
-  <img src="@/assets/images/Fond_image.svg" alt="Image de fond" class="w-auto absolute bottom-0 left-0 hidden md:block" style="max-width: 100%; max-height: 100%;" />
-  <!-- Votre image superposée, utilisez des unités vh pour la hauteur afin d'éviter d'agrandir la page -->
-  <img src="@/assets/images/mon_image.png" alt="Yannis Borel" class="w-auto absolute bottom-0 left-0 hidden md:block" style="height: 70vh; max-width: 100%;"
-  />
-</div>
+    <div class="relative overflow-hidden" style="height: 90vh;">
+      <!-- Image de fond -->
+      <img src="@/assets/images/Fond_image.svg" alt="Image de fond" class="w-auto absolute bottom-0 left-0 hidden lg:block" style="max-width: 100%; max-height: 100%;" />
+      <!-- Votre image superposée, utilisez des unités vh pour la hauteur afin d'éviter d'agrandir la page -->
+      <img src="@/assets/images/mon_image.png" alt="Yannis Borel" class="w-auto absolute bottom-0 left-0 hidden lg:block" style="height: 70vh; max-width: 100%;" />
+    </div>
 
-<div class="absolute bottom-52 w-full flex justify-end px-[400px]">
-  <button class="border-2 border-black text-black font-bold py-2 px-6 rounded-[30px] transition duration-150 ease-in-out hover:bg-black hover:text-white focus:outline-none">
-    Mes projets
-  </button>
-</div>
+    <div class="absolute bottom-52 w-full flex justify-center mx-auto">
+      <button class="border-2 justify-center flex items-center border-black text-black font-bold py-2 px-6 rounded-[30px] transition duration-150 ease-in-out hover:bg-black hover:text-white focus:outline-none">
+        Mes projets
+      </button>
+    </div>
 
     <div class="absolute bottom-0 right-0 p-4">
       <p class="text-[60px]" style="font-family: 'Coco-Bold'">Yannis Borel</p>
     </div>
     <div class="absolute bottom-0 p-4 left-1/2 transform -translate-x-1/2 animate-bounce">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-8 w-8 text-black"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M19 14l-7 7m0 0l-7-7m7 7V3"
-        />
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
       </svg>
     </div>
   </div>
   <div class="bg-black text-white py-8 px-4">
-    <h2 class="text-white text-6xl font-normal mb-8 text-center leading-normal tracking-[5.76px]" style="font-family: 'POLYA';">Qui suis-je ?</h2>
-    <div class="flex flex-col md:flex-row justify-center items-center gap-8 mb-8">
-  <div class="bg-black border-dotted border-2 border-white rounded-[40px] p-12 text-[30px] text-center max-w-[300px]">
-    <img src="@/assets/images/casquette.svg" class="block mx-auto text-4xl mb-2">
-    <span class="uppercase text-[26px] font-bold leading-normal tracking-wide">BUT MMI</span>
-  </div>
-  <div class="bg-black border-dotted border-2 border-white rounded-[40px] p-14 px-14 text-[30px] text-center max-w-[300px]">
-    <img src="@/assets/images/gateau.svg" class="block mx-auto text-4xl">
-    <span class="uppercase text-[26px] font-bold leading-normal tracking-wide">19 ans</span>
-  </div>
-  <div class="bg-black border-dotted border-2 border-white rounded-[40px] p-16 text-[30px] text-center max-w-[300px]">
-    <img src="@/assets/images/PACA.svg" class="block mx-auto text-4xl mb-2">
-    <span class="uppercase text-[26px] font-bold leading-normal tracking-wide">Paca</span>
-  </div>
-</div>
-    <p class="bg-black border-dotted border-2 border-white rounded-[40px] p-4 sm:p-8 mx-4 sm:mx-8 md:mx-[100px] lg:mx-[200px] xl:mx-[300px] text-[16px] sm:text-[20px] md:text-[24px] lg:text-[30px] font-bold text-white">
-  Passionné par le développement et la conception de solutions web innovantes, je suis un étudiant dynamique de 19 ans en deuxième année de BUT MMI, où j'affûte mes compétences en design et en programmation. Aspirant développeur web full stack, j'ai une soif d'apprendre constante et un engagement profond envers le code propre et les pratiques de développement agile.
-</p>
-
+    <h2 class="text-white text-6xl font-normal mb-10 text-center leading-normal tracking-[5.76px]" style="font-family: 'POLYA';">Qui suis-je ?</h2>
+    <div ref="dottedBorder" class="dotted-border mx-auto mt-[-20px] w-3/4 mb-16 overflow-hidden">
+      <div class="border-dotted-animation">
+        &nbsp;
+      </div>
+    </div>
+    <div class="flex flex-col md:flex-row justify-center items-center gap-8 m-8">
+      <div class="bg-black border-dotted border-2 border-white rounded-[40px] p-12 text-[30px] text-center max-w-[300px]">
+        <img src="@/assets/images/casquette.svg" class="block mx-auto text-4xl" />
+        <span class="uppercase text-[26px] font-bold leading-normal tracking-wide">BUT MMI</span>
+      </div>
+      <div class="bg-black border-dotted border-2 border-white rounded-[40px] py-12 px-16 mx-24 text-[30px] text-center max-w-[300px]">
+        <img src="@/assets/images/gateau.svg" class="block mx-auto text-4xl" />
+        <span class="uppercase text-[26px] font-bold leading-normal tracking-wide">19 ans</span>
+      </div>
+      <div class="bg-black border-dotted border-2 border-white rounded-[40px] p-14 text-[30px] text-center max-w-[300px]">
+        <img src="@/assets/images/PACA.svg" class="block mx-auto text-4xl" />
+        <span class="uppercase text-[26px] font-bold leading-normal tracking-wide">Paca</span>
+      </div>
+    </div>
+    <AnimationText />
+    
   </div>
 </template>
 
 <script setup>
 import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 import Header from "@/components/Header.vue";
 import LoadingScreen from '../components/LoadingScreen.vue';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { gsap } from 'gsap';
-
+import AnimationText from '@/components/AnimationText.vue';
 
 AOS.init();
 
+const dottedBorder = ref(null);
+
 onMounted(() => {
+  const dottedBorderEl = dottedBorder.value.querySelector('.border-dotted-animation');
+
+  gsap.set(dottedBorderEl, { backgroundPosition: '0 0' });
+
+  gsap.to(dottedBorderEl, {
+    backgroundPosition: '-100vw 0', // Déplacer le dégradé sur toute la largeur de la fenêtre
+    repeat: -1, // Répéter l'animation indéfiniment
+    duration: 10, // Durée de l'animation en secondes
+    ease: 'none', // Mouvement à vitesse constante
+  });
+
   gsap.from('.container', {
     duration: 1,
     y: 100,
@@ -91,8 +95,6 @@ onMounted(() => {
 </script>
 
 <style>
-
-
 @font-face {
   font-family: "Coco-Bold";
   src: url("../fonts/Coco-Bold.otf") format("opentype");
@@ -106,5 +108,17 @@ onMounted(() => {
   font-style: normal;
 }
 
+.dotted-border {
+  height: 2px;
+  background-image: radial-gradient(circle, white 1px, transparent 1px);
+  background-size: 10px 2px;
+  overflow: hidden; /* Cacher le débordement */
+}
 
+.border-dotted-animation {
+  height: 2px;
+  width: 100vw; /* Définir une largeur suffisante pour l'animation */
+  background-image: radial-gradient(circle, white 1px, transparent 1px);
+  background-size: 10px 2px;
+}
 </style>
